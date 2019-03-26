8efec9bd13d985b05c7ab7c7b45a9e3487673c53
rama41222 <dinushankanrg@gmail.com>
2019-03-27 00:26:18 +0530

    feat: server

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { log, ExpressAPILogMiddleware } = require('@rama41222/node-logger');
+
+const config = {
+    name: 'sample-express-app',
+    port: 3000,
+    host: '0.0.0.0',
+};
+
+const app = express();
+const logger = log({ console: true, file: false, label: config.name });
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(ExpressAPILogMiddleware(logger, { request: true }));
+
+app.get('/', (req, res) => {
+    res.status(200).send('hello world');
+});
+
+app.listen(config.port, config.host, (e)=> {
+    if(e) {
+        throw new Error('Internal Server Error');
+    }
+    logger.info(`${config.name} running on ${config.host}:${config.port}`);
+});
